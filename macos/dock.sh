de6d391489e4d4f569d7f76653f1b562595617fa
yongqi <zhangyongqi@leying365.com>
2023-02-03 10:17:06 +0800

    feat: update

--- macos/dock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
-
-killall Dock
